4fc6104f70c2c4cbd36e61a2cde5bb99480a032c
Md Obydullah <obydul@makpie.com>
2020-02-10 01:47:59 +0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <img alt="CI/CD" src="./assets/ci-cd-test-deploy-illustration.png" style="max-width: 500px;">
     <p>
       MyNotePaper.com Presents:<br><br>
-      <a href="https://www.mynotepaper.com/build-and-auto-deploy-vuejs-app-to-own-server-using-gitlab-ci-cd" target="_blank">Build & Auto Deploy Vue.js App to Own Server Using GitLab CI/CD</a>
+      <a href="https://www.mynotepaper.com/build-and-auto-deploy-vuejs-app-to-server-using-gitlab-ci-cd" target="_blank">Build & Auto Deploy Vue.js App to Own Server Using GitLab CI/CD</a>
     </p>
   </div>
 </template>
